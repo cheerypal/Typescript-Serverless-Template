066e8248a882d3f7a05e2bb022cc9a56e990b793
Euan <48190058+cheerypal@users.noreply.github.com>
2022-08-18 14:48:22 +0100

    Necessary Improvements to update the template

--- src/libs/db/index.ts
+++ src/libs/db/index.ts
@@ -1,11 +1,2 @@
 import DynamoDB from "aws-sdk/clients/dynamodb";
-import { DocumentClient } from "aws-sdk/clients/dynamodb";
-
-export const initDB = (): DocumentClient => {
-  if (process.env.STAGE === "local")
-    return new DynamoDB.DocumentClient({
-      region: process.env.REGION,
-      endpoint: process.env.LOCALHOST,
-    });
-  else return new DynamoDB.DocumentClient();
-};
+export const initDB = new DynamoDB.DocumentClient();
